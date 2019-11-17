03d272e49ee8beddac16e8d7a5dae0da7b8107fc
JosephRocha <kkw293@my.utsa.edu>
2019-11-17 17:21:53 -0600

    Added Form Functionality

--- js/app.js
+++ js/app.js
@@ -31,6 +31,40 @@ function main(){
     window.location.replace("signin.html");
   }
   document.body.innerHTML = document.body.innerHTML.replace(/\{username\}/g, "@"+cognitoUser.username);
+    
+$('form').submit(function (event){
+   console.log("WOW");
+    event.preventDefault();
+    var firstname = jQuery("#firstname").val();
+    var lastname = jQuery("#lastname").val();
+    console.log(firstname);
+    console.log(lastname);
+    $.ajax({
+        url: "https://ikx4tw4ty9.execute-api.us-east-1.amazonaws.com/dev/user",
+        type: "POST",
+        data: JSON.stringify({"body": {"accessToken": cognitoUser.signInUserSession.accessToken.jwtToken, 
+                                       "userId": cognitoUser.username,
+                                       "firstname": jQuery("#firstname").val(),
+                                       "lastname": jQuery("#lastname").val(),
+                                       "shirtsize": jQuery("#shirtsize").val(),
+                                       "school": jQuery("#school").val(),
+                                       "major": jQuery("#major").val(),
+                                       "classification": jQuery("#classification").val(),
+                                       "pronouns": jQuery("#pronouns").val(),
+                                       "ethnicity": jQuery("#ethnicity").val(),
+                                       "travel": jQuery("#travel").val(),
+                                       "dietaryinfo": jQuery("#dietaryinfo").val(),
+                                       "accomodations": jQuery("#accomodations").val(),
+                                       "track": jQuery("#track").val(),
+                                       "lookingforwardto": jQuery("#lookingforwardto").val(),
+                                      }}),
+        datatype: "json",
+        crossDomain: true,
+        contentType: 'application/json',
+        success: function (data) {
+            alert("Your application has been successfully received!");
+        }
+    });
+});
 }
-
 main();
